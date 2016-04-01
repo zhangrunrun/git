ce88c0e24b92f46165028325cbfe50e96db75152
DC012 <dc012@run.lan>
2016-04-01 14:03:19 +0800

    Initial Commit

---
+++ GITText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GITText
+//
+//  Created by DC012 on 16/4/1.
+//  Copyright © 2016年 DC012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
