4692cad31b18f405e16644e7fabce5db9b54059e
DC012 <dc012@run.lan>
2016-04-01 14:17:52 +0800

    brah2

--- GITText/ViewController.m
+++ GITText/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"hello wlorld");
+    NSLog(@"branch1");
+    NSLog(@"branch2");
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
